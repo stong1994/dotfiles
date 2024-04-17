b44cbbacc69004ef8ec954d87c2df66d47482497
stong <498327580@qq.com>
2024-04-17 14:56:52 +0800

    init

---
+++ fish/config.fish
@@ -0,0 +1,46 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+# set -g theme_color_scheme terminal-dark
+# set -g theme_color_scheme shellder
+# set -g fish_prompt_pwd_dir_length 1
+# set -g theme_display_user no
+# set -g theme_hide_hostname no
+# set -g theme_hostname no
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
+starship init fish | source
---
+++ fish/functions/fzf_change_directory.fish
@@ -0,0 +1,40 @@
+function _fzf_change_directory
+    fzf | perl -pe 's/([ ()])/\\\\$1/g' | read foo
+    if [ $foo ]
+        builtin cd $foo
+        commandline -r ''
+        commandline -f repaint
+    else
+        commandline ''
+    end
+end
+
+function fzf_change_directory
+    begin
+        echo $HOME/.config
+        find $(ghq root --all) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
+        # ls -ad $HOME/Developments/*/* | grep -v \.git
+    end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+end
+
+# function _fzf_change_directory
+#     local foo
+#     fzf | perl -pe 's/([ ()])/\\\\$1/g' | read -r foo
+#     if [ "$foo" ]; then
+#         builtin cd "$foo"
+#         commandline -r ''
+#         commandline -f repaint
+#     else
+#         commandline ''
+#     end
+# end
+#
+# function fzf_change_directory
+#     begin
+#         echo "$HOME/.config"
+#         ghq root --all | xargs -I{} find {} -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+#         ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v '/\.git$'
+#         # ls -ad "$HOME/Developments/"*/* | grep -v '/\.git$'
+#     end | sed 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+# end
