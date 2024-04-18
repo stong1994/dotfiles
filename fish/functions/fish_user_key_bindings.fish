a594eb7c89018b7f142843c6202ece7624782706
stong <498327580@qq.com>
2024-04-18 10:57:50 +0800

    vim like in command line

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,11 @@ function fish_user_key_bindings
 
     # vim-like
     bind \cl forward-char
+    bind \cw forward-word
+    bind \ca end-of-line
+    bind \ch backward-char
+    bind \cb backward-word
+    bind \ci beginning-of-line
 
     # prevent iterm2 from closing when typing Ctrl-D (EOF)
     bind \cd delete-char
