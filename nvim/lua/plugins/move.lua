1db87cd3309ea78de9a4cf40894eb37839706ba9
stong <498327580@qq.com>
2024-04-19 09:50:09 +0800

    move lines & blocks

--- nvim/lua/plugins/move.lua
+++ nvim/lua/plugins/move.lua
@@ -7,45 +7,13 @@ require("stong.move").setup({
     up = "<A-up>",
 
     line_left = "<A-left>",
-    line_right = "<A-right>",
+    line_right = "<A-rigt>",
     line_up = "<A-up>",
     line_down = "<A-down>",
   },
 })
 -- {
 return {
-  --   "fedepujol/move.nvim",
-  --   config = function()
-  --     require("move").setup({
-  --       line = {
-  --         enable = true, -- Enables line movement
-  --         indent = true, -- Toggles indentation
-  --       },
-  --       block = {
-  --         enable = true, -- Enables block movement
-  --         indent = true, -- Toggles indentation
-  --       },
-  --       word = {
-  --         enable = true, -- Enables word movement
-  --       },
-  --       char = {
-  --         enable = false, -- Enables char movement
-  --       },
-  --     })
-  --     vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", opts)
-  --     vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", opts)
-  --     vim.keymap.set("n", "<A-h>", ":MoveHChar(-1)<CR>", opts)
-  --     vim.keymap.set("n", "<A-l>", ":MoveHChar(1)<CR>", opts)
-  --     vim.keymap.set("n", "<leader>wf", ":MoveWord(1)<CR>", opts)
-  --     vim.keymap.set("n", "<leader>wb", ":MoveWord(-1)<CR>", opts)
-  --
-  --     -- Visual-mode commands
-  --     vim.keymap.set("v", "<A-j>", ":MoveBlock(1)<CR>", opts)
-  --     vim.keymap.set("v", "<A-k>", ":MoveBlock(-1)<CR>", opts)
-  --     vim.keymap.set("v", "<A-h>", ":MoveHBlock(-1)<CR>", opts)
-  --     vim.keymap.set("v", "<A-l>", ":MoveHBlock(1)<CR>", opts)
-  --   end,
-  -- },
   {
     "terryma/vim-expand-region",
   },
