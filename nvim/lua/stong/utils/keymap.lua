a391a01244c2c321ffcf3fa03ebc16f90c0a3502
stong <498327580@qq.com>
2024-04-24 02:13:06 +0800

    add map utils

---
+++ nvim/lua/stong/utils/keymap.lua
@@ -0,0 +1,31 @@
+local M = {}
+
+M.convert = function(mapping)
+  if not (vim.g.neovide or vim.g.neovim_gui) then
+    -- subsitute <D-letter> with <M-s>letter
+    mapping = string.gsub(mapping, "<D%-(.)>", "<M-s>%1")
+  end
+  return mapping
+end
+
+M.map = function(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, M.convert(lhs), rhs, opts)
+end
+
+M.default_map = function(mode, lhs, rhs, opts)
+  M.map(mode, lhs, rhs, table.insert({ noremap = true, silent = true }, opts))
+end
+
+M.default_map_n = function(lhs, rhs, opts)
+  M.default_map("n", lhs, rhs, opts)
+end
+
+M.default_map_x = function(lhs, rhs, opts)
+  M.default_map("x", lhs, rhs, opts)
+end
+
+M.default_map_nx = function(lhs, rhs, opts)
+  M.default_map({ "n", "x" }, lhs, rhs, opts)
+end
+
+return M
