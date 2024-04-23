47c7e80fc43dda71a1203f56d591eb8ed0648d7d
stong <498327580@qq.com>
2024-04-23 15:47:39 +0800

    add ghq to path and fzfp

--- fish/config.fish
+++ fish/config.fish
@@ -7,9 +7,12 @@ alias lla "ll -A"
 alias g git
 alias lg lazygit
 alias vim nvim
+alias cat='bat --paging=never'
+alias fzfp "fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 
 set -gx EDITOR nvim
 
+fish_add_path /Users/stong/ghq/github.com/stong1994/ghq
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
