89883fb38e476956535eb8ac39dcb7873ae07118
stong <498327580@qq.com>
2024-04-26 17:50:32 +0800

    refactor: keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,48 +4,47 @@
 local discipline = require("stong.discipline")
 discipline.cowboy()
 
-local keymap = vim.keymap
-local opts = { noremap = true, silent = true }
+local keymap = require("stong.utils.keymap")
 
 -- To resolve the conflict with Tab and Ctrl-i
-keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", opts)
-keymap.set("n", "<C-i>", "<C-i>", opts)
-keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", opts)
+keymap.default_map_n("<Tab>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
+keymap.default_map_n("<C-i>", "<C-i>", { desc = "Define C-i to resove conflict with Tab and Ctrl-i" })
+keymap.default_map_n("<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Last Buffer" })
 
 -- Copy content to the next line
-vim.keymap.set("n", "<C-p>", "m`o<ESC>p>`", opts)
+keymap.default_map_n("<C-p>", "m`o<ESC>p>`", { desc = "Copy content to the next line" })
 
 -- Buffer switch
-vim.keymap.set("n", "]b", [[<cmd>bnext<cr>]])
-vim.keymap.set("n", "[b", [[<cmd>bprevious<cr>]])
+keymap.default_map_n("]b", [[<cmd>bnext<cr>]])
+keymap.default_map_n("[b", [[<cmd>bprevious<cr>]])
 
 -- Quick Move
-vim.keymap.set("n", "J", "10j")
-vim.keymap.set("n", "K", "10k", { noremap = true })
+keymap.default_map_n("J", "10j")
+keymap.default_map_n("K", "10k")
 
 -- Select all
-keymap.set("n", "<C-a>", "gg<S-v>G")
+keymap.default_map_n("<C-a>", "gg<S-v>G", { desc = "Select all" })
 
 -- New Tab
-keymap.set("n", "te", ":tabedit<Return>", opts)
-keymap.set("n", "<tab-n>", ":tabnext<Return>", opts)
-keymap.set("n", "<tab-p>", ":tabprev<Return>", opts)
+keymap.default_map_n("te", ":tabedit<Return>", { desc = "Create a new tab" })
+keymap.default_map_n("<tab-n>", ":tabnext<Return>", { desc = "Next tab" })
+keymap.default_map_n("<tab-p>", ":tabprev<Return>", { desc = "Last tab" })
 
 -- Resize window
-vim.keymap.set("n", "=", [[<cmd>vertical resize +5<cr>]]) -- make the window biger vertically
-vim.keymap.set("n", "-", [[<cmd>vertical resize -5<cr>]]) -- make the window smaller vertically
-vim.keymap.set("n", "+", [[<cmd>horizontal resize +2<cr>]]) -- make the window bigger horizontally by pressing shift and =
-vim.keymap.set("n", "_", [[<cmd>horizontal resize -2<cr>]]) -- make the window smaller horizontally by pressing shift and -
+keymap.default_map_n("=", [[<cmd>vertical resize +5<cr>]], { desc = "increase window vertical size" }) -- make the window biger vertically
+keymap.default_map_n("-", [[<cmd>vertical resize -5<cr>]], { desc = "decrease window vertical size" }) -- make the window smaller vertically
+keymap.default_map_n("+", [[<cmd>horizontal resize +2<cr>]], { desc = "increase window horizontal size" }) -- make the window bigger horizontally by pressing shift and =
+keymap.default_map_n("-", [[<cmd>horizontal resize -2<cr>]], { desc = "decrease window horizontal size" }) -- make the window smaller horizontally by pressing shift and -
 
 --  Select Window
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.default_map_n("<C-h>", "<C-w>h", { desc = "Switch to left window" })
+keymap.default_map_n("<C-l>", "<C-w>l", { desc = "Swith to right window" })
+keymap.default_map_n("<C-j>", "<C-w>j", { desc = "Switch to below window" })
+keymap.default_map_n("<C-k>", "<C-w>k", { desc = "Swith to upper window" })
 
 -- Split window
-keymap.set("n", "ss", ":split<Return>", opts)
-keymap.set("n", "sv", ":vsplit<Return>", opts)
+keymap.default_map_n("ss", ":split<Return>", { desc = "Split window horizontal" })
+keymap.default_map_n("sv", ":vsplit<Return>", { desc = "Split window vertical" })
 -- keymap.set("n", "sd", "<Space-w>d", opts)
 
 ---- Page up/down
@@ -53,17 +52,17 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, false, true), mode, false)
 end
 
-keymap.set("n", "<C-u>", function()
+keymap.default_map_n("<C-u>", function()
   local height = math.floor(math.min(vim.fn.winheight(0) / 2, 20))
   feedkey(height .. "<C-u>", "n")
 end)
-keymap.set("n", "<C-d>", function()
+keymap.default_map_n("<C-d>", function()
   local height = math.floor(math.min(vim.fn.winheight(0) / 2, 20))
   feedkey(height .. "<C-d>", "n")
 end)
 
 -- Terminal
-keymap.set("n", "<A-CR>", '<Cmd>exe winheight(0)/3."split" | term<CR>', opts)
+keymap.default_map_n("<A-CR>", '<Cmd>exe winheight(0)/3."split" | term<CR>', { desc = "Open a new terminal" })
 -- keymap.set("n", "<A-CR>", function()
 --   require("toggleterm").exec_command("cmd='go run .'")
 -- end, { desc = "run project" })
