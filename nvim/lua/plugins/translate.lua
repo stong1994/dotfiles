ccbe5f9c0d45998a0a33495c5a63015c2bc17270
stong <498327580@qq.com>
2024-04-28 10:07:18 +0800

    translate

---
+++ nvim/lua/plugins/translate.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "voldikss/vim-translator",
+    keys = {
+      {
+        "tl",
+        "<cmd>Translate<cr>",
+        desc = "Translate",
+        mode = "n",
+      },
+    },
+  },
+}
