d386da009c7ae43a20263b7f5e5a898d5a85acc3
stong <498327580@qq.com>
2024-04-28 17:57:07 +0800

    notify

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -29,13 +29,34 @@ return {
   -- notice ui
   {
     "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true, -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      })
+    end,
     opts = function(_, opts)
       table.insert(opts.routes, {
         filter = {
           event = "notify",
           find = "No infomation available",
         },
-        opts = { skip = true },
+        opts = { skip = true }, -- no show "No infomation available"
       })
       opts.presets.lsp_doc_border = true
     end,
@@ -46,6 +67,9 @@ return {
     opts = {
       timeout = 5000,
     },
+    config = function()
+      require("notify").setup()
+    end,
   },
   -- statusline
   {
