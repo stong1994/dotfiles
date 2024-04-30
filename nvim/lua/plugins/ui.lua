5bab7a2dcf3d36efd0794a841ee0c81ec3583b03
stong <498327580@qq.com>
2024-04-30 16:34:29 +0800

    opt winbar

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -90,29 +90,89 @@ return {
     priority = 1200,
     config = function()
       local colors = require("solarized-osaka.colors").setup()
+      local devicons = require("nvim-web-devicons")
       require("incline").setup({
         highlight = {
           groups = {
             InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
             InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
             CursorLineNr = { guifg = colors.base4, guibg = colors.orange },
+            LineNr = { guifg = colors.base1, guibg = colors.base04 },
             lCursor = { guifg = colors.blue500, guibg = colors.yellow300 },
             Search = { guifg = colors.blue500, guibg = colors.yellow300 },
+            WinSeparator = { guifg = colors.green, guibg = colors.base04 },
+            Folded = { guibg = colors.violet500 },
+            CursorLineFolded = { guibg = colors.violet300 },
+            DiagnosticSignError = { guifg = colors.magenta900 },
           },
         },
         window = { margin = { vertical = 0, horizontal = 1 } },
         -- hide = {
         --   cursorline = true,
         -- },
+        -- to avoid render not too often, see https://github.com/b0o/incline.nvim/issues/69
+        debounce_threshold = {
+          falling = 75,
+          rising = 75,
+        },
         render = function(props)
           local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-          if vim.bo[props.buf].modified then
-            filename = "[+]" .. filename
+          if filename == "" then
+            filename = "[No Name]"
+          end
+          local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+          local function get_git_diff()
+            local icons = { removed = "", changed = "", added = "" }
+            local signs = vim.b[props.buf].gitsigns_status_dict
+            local labels = {}
+            if signs == nil then
+              return labels
+            end
+            for name, icon in pairs(icons) do
+              if tonumber(signs[name]) and signs[name] > 0 then
+                table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+              end
+            end
+            if #labels > 0 then
+              table.insert(labels, { "┊ " })
+            end
+            return labels
           end
 
-          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-          return { { icon, guifg = color }, { " " }, { filename } }
+          local function get_diagnostic_label()
+            local icons = { error = "", warn = "", info = "", hint = "" }
+            local label = {}
+
+            for severity, icon in pairs(icons) do
+              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
+              if n > 0 then
+                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+              end
+            end
+            if #label > 0 then
+              table.insert(label, { "┊ " })
+            end
+            return label
+          end
+
+          return {
+            { get_diagnostic_label() },
+            { get_git_diff() },
+            { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+            { filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
+            -- { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" }, no need to display the window num
+          }
         end,
+        --   render = function(props)
+        --     local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+        --     if vim.bo[props.buf].modified then
+        --       filename = "[+]" .. filename
+        --     end
+        --
+        --     local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+        --     return { { icon, guifg = color }, { " " }, { filename } }
+        --   end,
       })
     end,
   },
