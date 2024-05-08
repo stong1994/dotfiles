6d80e51a369f2a7d1a7dbb77746698a1145c685a
stong <498327580@qq.com>
2024-05-08 10:07:51 +0800

    format

--- nvim/lua/plugins/move.lua
+++ nvim/lua/plugins/move.lua
@@ -1,17 +1,17 @@
 -- consider another later: https://github.com/echasnovski/mini.nvim/blob/11920227ff26fb047d3f39bb666e9cdec523a190/lua/mini/move.lua
-require("stong.move").setup({
-  mappings = {
-    left = "<A-left>",
-    right = "<A-right>",
-    down = "<A-down>",
-    up = "<A-up>",
-
-    line_left = "<A-left>",
-    line_right = "<A-rigt>",
-    line_up = "<A-up>",
-    line_down = "<A-down>",
-  },
-})
+-- require("stong.move").setup({
+--   mappings = {
+--     left = "<A-left>",
+--     right = "<A-right>",
+--     down = "<A-down>",
+--     up = "<A-up>",
+--
+--     line_left = "<A-left>",
+--     line_right = "<A-rigt>",
+--     line_up = "<A-up>",
+--     line_down = "<A-down>",
+--   },
+-- })
 -- {
 return {
   {
