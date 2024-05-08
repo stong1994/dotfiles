97fe1368f81311c2e37efd3efa0bc5d4adb33d7a
stong <498327580@qq.com>
2024-05-08 10:10:08 +0800

    add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -9,12 +9,14 @@ alias lg lazygit
 alias vim nvim
 alias cat='bat --paging=never'
 alias fzfp "fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
-
+alias gitdiff 'git diff --name-only --diff-filter=d | xargs bat --diff'
+alias autodev '/Users/stong/Project/Personal/dev_helper/autodev/autodev.sh'
 set -gx EDITOR nvim
 
 set -gx LS_COLORS "$(vivid generate snazzy)" # colorized output of `tree`,`fd`..
 
 fish_add_path /Users/stong/ghq/github.com/stong1994/ghq
+fish_add_path /Users/stong/Project/Github/github.com/Raghav-rv28/kitty-sesh
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
