1f7ade209b667ff282855c2f9ae4a84e88a938d9
stong <498327580@qq.com>
2024-05-10 00:50:37 +0800

    nvim-spectre

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -488,4 +488,32 @@ return {
       end,
     },
   },
+  -- search and replace
+  {
+    "nvim-pack/nvim-spectre",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<leader>S",
+        '<cmd>lua require("spectre").toggle()<CR>',
+        desc = "Toggle Spectre",
+      },
+      {
+        "<leader>sw",
+        '<cmd>lua require("spectre").open_visual({select_word=true})<CR>',
+        desc = "Search current word",
+      },
+      {
+        "<leader>sw",
+        '<esc><cmd>lua require("spectre").open_visual()<CR>',
+        desc = "Search current word",
+        mode = "x",
+      },
+      {
+        "<leader>sp",
+        '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>',
+        desc = "Search current file",
+      },
+    },
+  },
 }
