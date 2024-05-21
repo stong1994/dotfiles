651227b5b5571d3d1534262f81c6fa9851bfaee7
stong <498327580@qq.com>
2024-05-21 14:04:08 +0800

    override gd & gr with fzfx

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -153,6 +153,8 @@ return {
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       -- disable a keymap
       keys[#keys + 1] = { "K", false }
+      keys[#keys + 1] = { "gd", false }
+      keys[#keys + 1] = { "gr", false }
     end,
 
     config = function()
