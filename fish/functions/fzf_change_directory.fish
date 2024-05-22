47692b702aa807cef4144c2cc2b5d8e981c808e6
stong <498327580@qq.com>
2024-05-22 18:07:35 +0800

    add lazy path to fzf_change_directory.fish

--- fish/functions/fzf_change_directory.fish
+++ fish/functions/fzf_change_directory.fish
@@ -15,6 +15,7 @@ function fzf_change_directory
         echo $HOME/.config
         # todo we use --hidden because we need search the directory with '.git', but with --hidden, we will search with all directory with prefix '.', wait fd to optimize it
         ghq root --all | xargs -I {} fd --type d --max-depth 6 --hidden .git {} | sed 's/\/\.git//'
+        $HOME/.local/share/nvim/lazy | xargs -I {} fd --type d --max-depth 4 --hidden .git {} | sed 's/\/\.git//'
         ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
