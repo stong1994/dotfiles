3299833db84669dc27163164ebe54899725fd6d2
stong <498327580@qq.com>
2024-07-04 15:20:08 +0800

    fix: correct directory navigation in fzf_change_directory
    Removed unnecessary commandline execution in _fzf_change_directory function. Also, fixed the directory path for nvim lazy loading in fzf_change_directory function.

--- fish/functions/fzf_change_directory.fish
+++ fish/functions/fzf_change_directory.fish
@@ -5,8 +5,6 @@ function _fzf_change_directory
         cd $foo
         commandline -r ''
         commandline -f repaint
-    else
-        commandline ''
     end
 end
 
@@ -16,7 +14,7 @@ function fzf_change_directory
         fd --type d --max-depth 1 . $HOME/.config
         # todo we use --hidden because we need search the directory with '.git', but with --hidden, we will search with all directory with prefix '.', wait fd to optimize it
         ghq root --all | xargs -I {} fd --type d --max-depth 6 --hidden --no-ignore .git {} | sed 's/\/\.git//'
-        $HOME/.local/share/nvim/lazy | xargs -I {} fd --type d --max-depth 4 --hidden .git {} | sed 's/\/\.git//'
+        echo $HOME/.local/share/nvim/lazy | xargs -I {} fd --type d --max-depth 4 --hidden .git {} | sed 's/\/\.git//'
         ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
 end
